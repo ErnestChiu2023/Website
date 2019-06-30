43b9d8bf2dc12cef8ec1971e53198a27b2485636
unknown <ernestchiu@hotmail.ca>
2019-06-30 10:36:26 -0400

    added work experience background

--- ernest_chiu/src/components/we.js
+++ ernest_chiu/src/components/we.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../css/about.css";
 import Sitenav from "./sitenav";
+import Radium, { StyleRoot } from "radium";
+import { fadeInUp } from "react-animations";
+import "../css/we.css";
+
+const styles = {
+  fadeInUp: {
+    animation: "x .7s",
+    animationName: Radium.keyframes(fadeInUp, "fadeInUp")
+  }
+};
 
 function WorkExperience() {
   return (
-    <div className="WorkExperience">
+    <StyleRoot>
       <Sitenav page={"Work Experience"} />
-      <h1>WorkExperience</h1>
-    </div>
+      <div style={styles.fadeInUp}>
+        <div className="WorkExperience">
+          <h1>WorkExperience</h1>
+        </div>
+      </div>
+    </StyleRoot>
   );
 }
 
