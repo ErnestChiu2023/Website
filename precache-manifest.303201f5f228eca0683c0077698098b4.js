72e0996014f4892e6dc386903b7455602dec3ce0
ernest chiu <ernestchiu@hotmail.ca>
2020-09-21 20:57:18 -0400

    Updates

---
+++ precache-manifest.303201f5f228eca0683c0077698098b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4070f4b10606c1482e91c0483096d03",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "92c3b363390674ed5220",
+    "url": "/Website/static/css/2.84339573.chunk.css"
+  },
+  {
+    "revision": "e8eb3dac780e934bf85d",
+    "url": "/Website/static/css/main.ce25105e.chunk.css"
+  },
+  {
+    "revision": "92c3b363390674ed5220",
+    "url": "/Website/static/js/2.094ed4cd.chunk.js"
+  },
+  {
+    "revision": "e8eb3dac780e934bf85d",
+    "url": "/Website/static/js/main.d38c3433.chunk.js"
+  },
+  {
+    "revision": "11da32b4931af81a4237",
+    "url": "/Website/static/js/runtime~main.9e351133.js"
+  },
+  {
+    "revision": "3df36fb91dd381bb2d15c80f11c71b35",
+    "url": "/Website/static/media/ErnestChiuResume.3df36fb9.pdf"
+  },
+  {
+    "revision": "43fa6ae805874756f5d9958157832af9",
+    "url": "/Website/static/media/ErnestChiuResume.43fa6ae8.jpg"
+  },
+  {
+    "revision": "b1a429d4cdf5d33c405b9b6d3c9e7864",
+    "url": "/Website/static/media/picture.b1a429d4.jpeg"
+  },
+  {
+    "revision": "09bbf2a3c2912d0a8dfa41396b9638eb",
+    "url": "/Website/static/media/pig.09bbf2a3.png"
+  },
+  {
+    "revision": "6446b5b8a121b078e61dd23611dfded9",
+    "url": "/Website/static/media/tasks.6446b5b8.png"
+  }
+]);
